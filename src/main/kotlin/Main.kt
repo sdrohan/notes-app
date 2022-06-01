b2bb02d4bb401934caa20f2f9a32e97d233204f6
Siobhan Drohan <sdrohan@wit.ie>
2022-06-01 16:58:37 +0100

    Implement ScannerInput utility for robust console IO

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
+import utils.ScannerInput
 import java.lang.System.exit
-import java.util.*
-
-val scanner = Scanner(System.`in`)
 
 fun main(args: Array<String>) {
     runMenu()
 }
 
 fun mainMenu() : Int {
-    print(""" 
+    return ScannerInput.readNextInt(""" 
          > ----------------------------------
          > |        NOTE KEEPER APP         |
          > ----------------------------------
@@ -21,7 +19,6 @@ fun mainMenu() : Int {
          > |   0) Exit                      |
          > ----------------------------------
          > ==>> """.trimMargin(">"))
-    return scanner.nextInt()
 }
 
 fun runMenu() {
