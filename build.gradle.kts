c06a7165d6c284db90cef281c1006f6020d69882
Siobhan Drohan <sdrohan@wit.ie>
2022-06-01 17:13:50 +0100

    Add Logging capabilities to function calls

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // dependencies for logging
+    implementation("io.github.microutils:kotlin-logging:2.1.23")
+    testImplementation("org.slf4j:slf4j-simple:1.7.36")
 }
 
 tasks.test {
